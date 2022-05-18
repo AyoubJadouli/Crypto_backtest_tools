a84dd133aa96eb21f951180f0280ad056a983ef2
AyoubJadouli <46377718+AyoubJadouli@users.noreply.github.com>
2022-05-18 14:53:14 +0100

    5m or more

--- database/download_data.js
+++ database/download_data.js
@@ -140,7 +140,7 @@ let exchange = new ccxt.binance({ enableRateLimit: true })
 pair_list = coin_list['Binance_USDT_HALAL']
 
 // --- Edit timeframe list and start date here ---
-timeframe_list = ['1m','5m','15m','1h','4h','1d']
+timeframe_list = ['5m','15m','1h','4h','1d']
 start_date = "01-06-2017"
 
 
